63af8fb37d7bffc22b5615d238b27148f59ad69f
PrudhviNaidu <prudwi@sails.solutions>
2020-01-02 19:54:47 +0530

    changes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'car-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit {
+      jVersion: any;
 
-  constructor() { }
+  constructor() {
+      this.jVersion = environment.version;
+  }
 
   ngOnInit() {
   }
